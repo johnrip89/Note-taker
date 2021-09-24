e237490b7e9763e6b9ad48d083acaf616577bf38
johnrip89 <lebronjohn89@gmail.com>
2021-09-24 17:52:36 -0600

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# Note-taker
+# Note Taker  
+
+  ## Table of Contents
+  -[Description](#description)
+
+  -[Installation](#installation)
+
+  -[Usage](#usage)
+
+  -[Contributors](#contributors) 
+
+  -[Questions](#questions)
+
+  ## Description
+  Make small notes that can be saved, edited, and deleted.
+
+  ## Installation
+  'npm inquirer' will need to be installed.
+
+  ## Usage
+  Run 'npm start' in the terminal and run the url http://localhost: with the given port number provided in the terminal.  
+
+  ## Contributors
+  John Ripplinger 
+
+  ## Questions
+  https://github.com/johnrip89
+
+  lebronjohn89@gmail.com
+  
--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -1,11 +1,27 @@
 const router = require('express').Router();
+const { validateNote, createNewNote, deleteNote } = require('../../lib/notes');
+const { notes } = require('../../db/db.json');
 
-const { db } = require('./db/db.json');
-const { animals } = require('./db/animals');
+router.get('/notes', (req, res) => {
+    let results = notes;
+    res.json(results);
+});
 
-router.get('/animals', (req, res) => {    
-    let results = animals;
-    res.send(results);
+router.delete('/notes/:id', (req, res) => {  
+    let deleted = req.params.id; 
+    deleteNote(deleted, notes); 
+    res.json(notes);
+});    
+
+router.post('/notes', (req, res) => {    
+    req.body.id = notes.length.toString();
+
+    if (!validateNote(req.body)) {
+        res.status(400).send('The note is not properly formatted.');
+    } else {
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    }
 });
 
 module.exports = router;
